c24a4594543a28752faf0e3ca9c2ce923982e3f0
tooksTanmay <97392494+tooksTanmay@users.noreply.github.com>
2022-01-28 15:53:53 +0530

    secondScriptCommit

--- script.js
+++ script.js
@@ -23,15 +23,17 @@ burger.addEventListener("click", () => {
 
       //burger-lines
       //line1
-      line1.style.rotate = '45deg';
+      line1.style.rotate = "45deg";
+      line1.setAttribute('style', "rotate: 45deg !important");
       line1.style.width = "30px";
-      line1.style.translate = '0px 9px';
+      line1.style.translate = "0px 9px";
       //line2
       line2.style.width = "0px";
       //line3
-      line3.style.rotate = '-45deg';
+      line3.style.rotate = "-45deg";
+      line3.setAttribute('style', "rotate: -45deg !important");
       line3.style.width = "30px";
-      line3.style.translate = '0px -9px';
+      line3.style.translate = "0px -9px";
 
       //hopeDiv
       hopeDiv.style.display = "none";
@@ -43,17 +45,20 @@ burger.addEventListener("click", () => {
 
       //burger-lines
       //line1
-      line1.style.rotate = '0deg';
-      line1.style.translate = '0px';
+      line1.style.rotate = "0deg";
+      line1.setAttribute('style', "rotate: 0deg !important");
+      line1.style.translate = "0px";
       line1.style.width = "26px";
       //line2
       line2.style.width = "26px";
       //line3
-      line3.style.rotate = '0deg';
+      line3.style.rotate = "0deg";
+      line3.setAttribute('style', "rotate: 0deg !important");
       line3.style.width = "26px";
-      line3.style.translate = '0px';
+      line3.style.translate = "0px";
 
       //hopeDiv
       hopeDiv.style.display = "flex";
    }
 });
+
